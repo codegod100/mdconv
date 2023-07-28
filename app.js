fcc9019653265d5a8fafe44b933e138e4ed9cac9
V <veronika.m.winters@gmail.com>
2023-07-28 18:41:53 +0000

    remove logging

--- app.js
+++ app.js
@@ -5,13 +5,10 @@ let out = ""
 const dir = argv[2]
 let files = fs.readdirSync(dir).reverse()
 const converter = new showdown.Converter()
-//console.log(files)
 for(const file of files){
 	if(!file.includes(".md")) continue
 	let body = fs.readFileSync(`${dir}/${file}`).toString()
-	//console.log(body)
 	let html = converter.makeHtml(body)
-	//console.log(html)
 	out = out.concat(`<h1>${file}</h1>${html}`)
 }
 console.log(out)
